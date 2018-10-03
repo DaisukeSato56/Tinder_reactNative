e9b21c509c441f77241741478726bf23968c2f0f
佐藤大介 <workshift56@gmail.com>
2018-10-03 23:36:49 +0900

    facebookログイン機能を追加

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles";
-import { Text, View } from "react-native";
+import { Text, View, Alert } from "react-native";
 import { connect } from "react-redux";
 import { login } from "../redux/actions";
 
@@ -9,7 +9,23 @@ class Home extends React.Component {
 
   componentWillMount() {
     this.props.login("gahaha");
+    this.login();
   }
+
+  login = async () => {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+      "173023876951305",
+      {
+        permission: ["public_profile"]
+      }
+    );
+    if (type === "success") {
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      );
+      Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+    }
+  };
 
   render() {
     return (
---
+++ screens/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles";
+import { Text, View } from "react-native";
+
+class Login extends React.Component {
+  state = {};
+
+  componentWillMount() {}
+
+  render() {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+}
+
+export default Login;
